632d0fe9a46068a73609802e1411eb981d7e58ef
GAVIN MULHERN <gavin.mulhern.2020@mumail.ie>
2021-11-30 23:12:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,20 @@ app.post('/newroom', async (req,res) => {
     res.redirect('/'); // Redirect to home page
 })
 
+app.get('/deleteroom', async function(req, res){
+    var roomID = "cb831c00-95e1-48c7-bb11-4a9b8f949bb7"; // Id for the room to delete TODO: Get automatically from room
+    var deleteRoom = db.ref('rooms/' + roomID);
+    deleteRoom.remove()
+        .then(function() {
+            console.log("Remove succeeded.")
+        })
+        .catch(function(error) {
+            console.log("Remove failed: " + error.message)
+        });
+    res.redirect('/'); // Redirect to home page
+
+})
+
 // Create a room in Firebase
 function writeRoomData(userId, name, price, details, imageUrl) {
     db.ref("rooms/"+userId).set({
@@ -77,38 +91,38 @@ function getData(){
 function retrieveData(data) {
 
     // TODO: Create ul in HTML with id = room-List
-        const roomList = document.querySelector('#room-list');
-    
-        var roomListings = selectAll('.roomListing');
-        for (var i = 0; i < roomListings.length; i++) {
-            roomListings[i].remove();
-        }
-    
-        var rooms = data.val();
-        var keys = Object.keys(rooms);
-    
-        for(var i = 0; i < keys.length; i++) {
-            var k = keys [i];
-            var details = rooms [k].details;
-            var name = rooms [k].name;
-            var picture = rooms [k].picture;
-            var price = rooms [k].price;
-    
-            var li = createElement('li', details + ' ' + name + ' ' + picture + ' ' + price);
-            li.class('roomListing');
-            li.parent('room-list');
-        }
+    const roomList = document.querySelector('#room-list');
+
+    var roomListings = selectAll('.roomListing');
+    for (var i = 0; i < roomListings.length; i++) {
+        roomListings[i].remove();
     }
-    
-    // Update data
-    function updateData (colName, docID, det, name, pic, price) {
-        db.collection(colName).doc(docID).set({
+
+    var rooms = data.val();
+    var keys = Object.keys(rooms);
+
+    for(var i = 0; i < keys.length; i++) {
+        var k = keys [i];
+        var details = rooms [k].details;
+        var name = rooms [k].name;
+        var picture = rooms [k].picture;
+        var price = rooms [k].price;
+
+        var li = createElement('li', details + ' ' + name + ' ' + picture + ' ' + price);
+        li.class('roomListing');
+        li.parent('room-list');
+    }
+}
+
+// Update data
+function updateData (colName, docID, det, name, pic, price) {
+    db.collection(colName).doc(docID).set({
         details: det,
         name: name,
         picture: pic,
         price: price
-        })
-    }
+    })
+}
 
 // listen on port 3000
 app.listen(3000, ()=>{
@@ -121,5 +135,5 @@ function display() {
 }
 
 function hide() {
-  propertyForm.style.display = "none";
+    propertyForm.style.display = "none";
 }
